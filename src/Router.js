88dd1cfdbab8e7c2145827976cc24cca08d547ff
Sasheem <sts12@me.com>
2018-05-27 16:21:32 -0400

    set up router file to handle navigatino

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+// define all the different routes a user can make in this app
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Scene key="root">
+        <Scene key="login" component={LoginForm} title="Please Login" />
+      </Scene>
+    </Router>
+  );
+};
+
+export default RouterComponent;
