90e5dd45e31b9c5ceec810ba1da70f511732e65d
Sasheem <sts12@me.com>
2018-05-28 12:46:14 -0400

    combined EmployeeForm reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
+import EmployeeFormReducer from './EmployeeFormReducer';
 
 export default combineReducers({
   // this is the property of state that I will be producing
-  auth: AuthReducer
+  auth: AuthReducer,
+  employeeForm: EmployeeFormReducer
 });
