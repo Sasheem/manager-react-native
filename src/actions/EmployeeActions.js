c508520f302de581dc8d32396d63f846a8dade41
Sasheem <sts12@me.com>
2018-05-29 13:22:55 -0400

    employeeCreate action updates employee list in firebase for currently logged in user

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { EMPLOYEE_UPDATE } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -7,3 +9,21 @@ export const employeeUpdate = ({ prop, value }) => {
   };
 };
 // email, phone, and shift fields
+
+export const employeeCreate = ({ name, phone, shift }) => {
+
+  // gain access to the user currently logged into this application
+  const { currentUser } = firebase.auth();
+
+  // don't really need to do anything b/c its purpose is to save data to Firebase
+  // using redux thunk to break the rules
+  // by exploiting the fact we return a function but not really returning
+  // what redux desires
+  return (dispatch) => {
+    // get access to our firebase db, location where we store our data
+    // then make a reference to the path to our json data structure
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => Actions.pop());
+  };
+};
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+
+  onButtonPress() {
+    // continue here. Creating Records With Firebase
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+  }
+
   render() {
     return (
       <Card>
@@ -43,7 +51,7 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Create
           </Button>
         </CardSection>
@@ -66,4 +74,4 @@ const mapStateToProps = (state) => {
 }
 
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
